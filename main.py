f58fd22727c4539442dcefd18e5db1b300bb865d
BalfurRaine <micahb8850@gmail.com>
2022-11-10 13:40:27 -0600

    File moving. Need to rename files.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from datetime import date
-from pathlib import Path
 import os
+import shutil
 
 
 current_date = date.today()
 home_directory = os.path.expanduser('~')
-date_format = '%A, %B %d, %Y'
+date_format = '%B %d, %Y'
+formatted_date = current_date.strftime(date_format)
 folder_path = os.path.join(home_directory, "Desktop", "Hunting Camera")
-current_folder_path = os.path.join(folder_path, current_date.strftime(date_format))
+current_folder_path = os.path.join(folder_path, formatted_date)
 
 if not os.path.exists(current_folder_path):
     os.makedirs(current_folder_path)
 
-print("Today's date is ", current_date)
+for file in os.listdir("D:/"):
+    if file.__contains__(".jpg") or file.__contains__(".mov"):
+        shutil.move("D:/" + file, current_folder_path)
+
+print(os.listdir("D:/"))
