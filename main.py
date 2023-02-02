e3f16bf7b0b471eeac9b9f019cce717367c3f2a6
CatchOption <micahb8850@gmail.com>
2023-02-01 21:25:23 -0600

    Picture Migration script.

--- main.py
+++ main.py
@@ -14,20 +14,21 @@ current_folder_path = os.path.join(folder_path, formatted_date)
 if not os.path.exists(current_folder_path):
     os.makedirs(current_folder_path)
 
-if os.listdir("F:/").__contains__("DCIM"):
-    if os.listdir("F:/DCIM").__contains__("100MFCAM"):
-        for file in os.listdir("F:/DCIM/100MFCAM"):
-            if file.__contains__(".jpg") or file.__contains__(".mov"):
-                source_file = os.path.join("F:/DCIM/100MFCAM", file)
-                destination_file = os.path.join("F:/DCIM/100MFCAM", formatted_date + " " + file)
+if os.listdir("E:/").__contains__("DCIM"):
+    if os.listdir("E:/DCIM").__contains__("100MFCAM"):
+        for file in os.listdir("E:/DCIM/100MFCAM"):
+            current_file = os.path.splitext(file)
+            if current_file[1] == (".jpg" or ".JPG") or ".mov" or ".AVI":
+                source_file = os.path.join("E:/DCIM/100MFCAM", file)
+                destination_file = os.path.join("E:/DCIM/100MFCAM", formatted_date + " " + file)
                 os.rename(source_file, destination_file)
                 shutil.move(destination_file, current_folder_path)
-                ctypes.windll.user32.MessageBoxW(0, "Pictures have been moved successfully!", "Files Moved!", 0)
-    if os.listdir("F:/DCIM").__contains__("100STLTH"):
-        for file in os.listdir("F:/DCIM/100STLTH"):
-            if file.__contains__(".jpg") or file.__contains__(".mov"):
-                source_file = os.path.join("F:/DCIM/100STLTH", file)
-                destination_file = os.path.join("F:/DCIM/100STLTH", formatted_date + " " + file)
+    if os.listdir("E:/DCIM").__contains__("100STLTH"):
+        for file in os.listdir("E:/DCIM/100STLTH"):
+            current_file = os.path.splitext(file)
+            if current_file[1] == (".jpg" or ".JPG") or ".mov" or ".AVI":
+                source_file = os.path.join("E:/DCIM/100STLTH", file)
+                destination_file = os.path.join("E:/DCIM/100STLTH", formatted_date + " " + file)
                 os.rename(source_file, destination_file)
                 shutil.move(destination_file, current_folder_path)
-                ctypes.windll.user32.MessageBoxW(0, "Pictures have been moved successfully!", "Files Moved!", 0)
+ctypes.windll.user32.MessageBoxW(0, "Pictures have been moved successfully!", "Files Moved!", 0)
